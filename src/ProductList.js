efcc154529b6b196b4874f3e28cf612eb28f5602
Hufng2503 <hungnguyen2503th@gmail.com>
2022-12-02 19:34:58 +0700

    bai1

---
+++ src/ProductList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './ProductList.css';
+export default function ProductList() {
+  return (
+    <div className='wrapper'>
+        <div className='div1'>ProductList </div>
+        <div id='div2'>bbbb</div>
+        <div>aaaa</div>
+        <div>cccc</div>
+        <>hello mn!</>
+        </div>
+    
+    
+  );
+}
